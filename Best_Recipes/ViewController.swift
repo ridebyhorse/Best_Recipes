715ecea6591451f6292ffafc6063ed2fbb4ff76a
Maria Nesterova <ridebyhorse@gmail.com>
2024-06-30 12:35:29 +0300

    Initial Commit

---
+++ Best_Recipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Best_Recipes
+//
+//  Created by Мария Нестерова on 30.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
