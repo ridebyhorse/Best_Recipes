67ec2e6c06744bf93659b97a038f11e63f6a2aea
STASART <121148380+STASART@users.noreply.github.com>
2024-07-04 02:27:04 +0300

    addDififfableDataSource

--- Best_Recipes/HomeModule/ViewModel/RecipesCellViewModel.swift
+++ Best_Recipes/HomeModule/ViewModel/RecipesCellViewModel.swift
@@ -6,19 +6,21 @@
 //
 import Foundation
 
-struct RecipesCellViewModel: Hashable {
-    let id: Int
+struct RecipesCellViewModel: Hashable, Identifiable {
+    let id = UUID()
+    let recipeid: Int
     let raiting: Double
     let recipeImage: URL
     let recipeName: String
-    let isFavorite: Bool
+    var isFavorite: Bool
     let avtorImage: String
     let avtorName: String
+    let coockingTime: Int
     let didSelect: (() -> Void)?
     let favoriteHandler: (() -> Void)?
     
     static func == (lhs: RecipesCellViewModel, rhs: RecipesCellViewModel) -> Bool {
-        lhs.hashValue == lhs.hashValue
+           return lhs.id == rhs.id &&  lhs.isFavorite == rhs.isFavorite
     }
     
     func hash(into hasher: inout Hasher) {
@@ -26,3 +28,45 @@ struct RecipesCellViewModel: Hashable {
         hasher.combine(id)
     }
 }
+//
+//struct RecipesCategoryCellViewModel: Hashable, Identifiable {
+//    let id = UUID()
+//    let recipeid: Int
+//    let raiting: Double
+//    let recipeImage: URL
+//    let recipeName: String
+//    var isFavorite: Bool
+//    
+//    let didSelect: (() -> Void)?
+//    let favoriteHandler: (() -> Void)?
+//    
+//    static func == (lhs: RecipesCategoryCellViewModel, rhs: RecipesCategoryCellViewModel) -> Bool {
+//           return lhs.id == rhs.id &&  lhs.isFavorite == rhs.isFavorite
+//    }
+//    
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(isFavorite)
+//        hasher.combine(id)
+//    }
+//}
+//
+//struct RecipesCategoryCellViewModel: Hashable, Identifiable {
+//    let id = UUID()
+//    let recipeid: Int
+//    let raiting: Double
+//    let recipeImage: URL
+//    let recipeName: String
+//    var isFavorite: Bool
+//    let coockingTime: Int
+//    let didSelect: (() -> Void)?
+//    let favoriteHandler: (() -> Void)?
+//    
+//    static func == (lhs: RecipesCategoryCellViewModel, rhs: RecipesCategoryCellViewModel) -> Bool {
+//           return lhs.id == rhs.id &&  lhs.isFavorite == rhs.isFavorite
+//    }
+//    
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(isFavorite)
+//        hasher.combine(id)
+//    }
+//}
