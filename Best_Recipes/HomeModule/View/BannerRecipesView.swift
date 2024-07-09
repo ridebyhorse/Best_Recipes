2cabb10c974511da510df28b1a6523e4ba622892
STASART <121148380+STASART@users.noreply.github.com>
2024-07-10 02:19:36 +0300

    Almost final Layout

--- Best_Recipes/HomeModule/View/BannerRecipesView.swift
+++ Best_Recipes/HomeModule/View/BannerRecipesView.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 final class BannerRecipesView: CellView, Configurable {
-    let precipeImageView = UIImageView()
+    private let precipeImageView = UIImageView()
     private let nameRecipeLabel = UILabel()
     
     
@@ -53,7 +53,6 @@ final class TitleRecipesView: CellView, Configurable {
     
     func update(with model: SeeAll?) {
         guard let model = model else {
-            stackView.removeFromSuperview()
             label.text = nil
             seeAllHandler = nil
             button.isHidden = true
@@ -61,7 +60,6 @@ final class TitleRecipesView: CellView, Configurable {
         }
         
         label.text = model.headerName
-        
     }
     
     override func configure() {
@@ -85,21 +83,35 @@ final class TimeCircleRecipesView: CellView, Configurable {
     private let nameRecipeLabel = UILabel()
     private let cookingTimeLabel = UILabel()
     private let cookingTimeTitleLabel = UILabel()
-    
-    
+    private let timeStackView = UIStackView()
+    private let favoriteButton = createButton()
     
     func update(with model: RecipesCellViewModel?) {
         guard let model = model else {
             precipeImageView.update(with: nil)
-            //precipeImageView.layer.cornerRadius = precipeImageView.frame.height / 2
+            nameRecipeLabel.text = nil
+            cookingTimeTitleLabel.text = nil
+            cookingTimeLabel.text = nil
+            favoriteButton.setImage(.bookmark, for: .normal)
             return
         }
         
         precipeImageView.update(with: .init(url: model.recipeImage , cornerRadius: precipeImageView.frame.height / 2))
         nameRecipeLabel.text = model.recipeName
+        
+        cookingTimeLabel.text = String(model.coockingTime) + " Mins"
+        cookingTimeTitleLabel.text = "Time"
         nameRecipeLabel.numberOfLines = 2
         nameRecipeLabel.font = .custom(font: .bold, size: 14)
         nameRecipeLabel.textAlignment = .center
+       
+        
+        cookingTimeTitleLabel.font = .custom(font: .regular, size: 12)
+        cookingTimeTitleLabel.textColor = .darkGreyApp
+        cookingTimeLabel.font = .custom(font: .bold, size: 12)
+        
+        let imageButton: UIImage = !model.isFavorite ? .bookmarkActive : .bookmark
+        favoriteButton.setImage(imageButton, for: .normal)
         
         
     }
@@ -107,7 +119,7 @@ final class TimeCircleRecipesView: CellView, Configurable {
     override func configure() {
         setupSubView()
         setupContraints()
-        
+        setupStackView()
         backgroundView.backgroundColor = .lightGreyApp
         backgroundView.layer.cornerRadius = 10
     }
@@ -122,7 +134,7 @@ final class TimeCircleRecipesView: CellView, Configurable {
 
 private extension TimeCircleRecipesView {
     func setupSubView() {
-        addSubviews(backgroundView, precipeImageView, nameRecipeLabel)
+        addSubviews(backgroundView, precipeImageView, nameRecipeLabel, timeStackView, favoriteButton)
     }
     
     func setupContraints() {
@@ -138,10 +150,36 @@ private extension TimeCircleRecipesView {
         }
         
         nameRecipeLabel.snp.makeConstraints { make in
-            make.top.equalTo(precipeImageView.snp.bottom).offset(20)
+            make.top.equalTo(precipeImageView.snp.bottom).offset(12)
             make.leading.trailing.equalToSuperview().inset(15)
         }
+        
+        timeStackView.snp.makeConstraints { make in
+            make.leading.bottom.equalToSuperview().inset(12)
+            make.bottom.equalToSuperview().inset(11)
+        }
+        
+        favoriteButton.snp.makeConstraints { make in
+            make.trailing.bottom.equalToSuperview().inset(10)
+        }
+        
     }
+    
+    func setupStackView() {
+        timeStackView.addArrangedSubviews(cookingTimeTitleLabel, cookingTimeLabel)
+        timeStackView.axis = .vertical
+        timeStackView.spacing = 5
+        timeStackView.distribution = .fillEqually
+    }
+    
+    static func createButton() -> UIButton {
+        let button = UIButton()
+        button.tag = 0
+        button.setImage(.bookmark, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }
+    
 }
 
 
@@ -157,20 +195,17 @@ final class CategoryView: CellView {
             make.leading.trailing.equalToSuperview().inset(10)
         }
         
-        
         layer.cornerRadius = 10
-        
     }
   
     override func cellSelected(_ isSelected: Bool) {
-        print(isSelected)
         backgroundColor = isSelected ? .redApp : .white
         categoryLabel.textColor = isSelected ? .white : .redCategoryText
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        print(frame)
+        
     }
 }
 
@@ -181,6 +216,139 @@ extension CategoryView: Configurable {
             return
         }
         categoryLabel.text = model.headerName
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    UINavigationController(rootViewController: ModuleFactory().createHomeModule(flowHandler: nil))
+}
+
+
+
+final class RecentRecipeView: CellView {
+    private let precipeImageView = UIImageView()
+    private let nameRecipeLabel = UILabel()
+    private let avtorNameLabel = UILabel()
+    
+    override func configure() {
+        setupSubView()
+        setupContraints()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
+
+private extension RecentRecipeView {
+    
+    func setupSubView() {
+        addSubviews(precipeImageView, nameRecipeLabel, avtorNameLabel)
+    }
+    
+    func setupContraints() {
+        precipeImageView.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+            make.height.equalTo(nameRecipeLabel.snp.width)
+        }
+        
+        nameRecipeLabel.snp.makeConstraints { make in
+            make.top.equalTo(precipeImageView.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        avtorNameLabel.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(5)
+        }
+        
+    }
+}
+
+extension RecentRecipeView: Configurable {
+    
+    func update(with model: RecipesCellViewModel?) {
+        guard let model = model else {
+            precipeImageView.update(with: nil)
+            avtorNameLabel.text = nil
+            avtorNameLabel.text = nil
+            return
+        }
         print(frame)
+        precipeImageView.update(with: .init(url: model.recipeImage , cornerRadius: 10))
+        nameRecipeLabel.text = model.recipeName
+        avtorNameLabel.text = model.avtorName
+       
+       
+        nameRecipeLabel.font = .custom(font: .bold, size: 14)
+        nameRecipeLabel.textAlignment = .left
+        nameRecipeLabel.numberOfLines = 2
+        
+        avtorNameLabel.font = .custom(font: .regular, size: 12)
+        avtorNameLabel.textColor = .darkGreyApp
+        avtorNameLabel.font = .custom(font: .bold, size: 12)
+        
+        
+    }
+}
+
+
+final class CountryCategoryView: CellView {
+    private let countryImageView = UIImageView()
+    private let countryNameLabel = UILabel()
+ 
+    
+    override func configure() {
+        setupSubView()
+        setupContraints()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        countryImageView.layer.cornerRadius = countryImageView.frame.height / 2
+        countryImageView.clipsToBounds = true
+        print(frame)
+    }
+}
+
+private extension CountryCategoryView {
+    
+    func setupSubView() {
+        addSubviews(countryImageView, countryNameLabel)
+    }
+    
+    func setupContraints() {
+        countryImageView.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+            make.height.equalTo(100)
+            make.width.height.equalTo(self.snp.width)
+        }
+        
+        countryNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(countryImageView.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().inset(15)
+            
+        }
+    }
+}
+
+extension CountryCategoryView: Configurable {
+    
+    func update(with model: Country?) {
+        guard let model = model else {
+            countryImageView.update(with: nil)
+            countryNameLabel.text = nil
+            return
+        }
+        
+        countryImageView.update(with: .init(image: .british , cornerRadius:  countryImageView.frame.height / 2))
+        countryNameLabel.text = "Country"
+        
+       
+       
+        countryNameLabel.font = .custom(font: .bold, size: 14)
+        countryNameLabel.textAlignment = .center
+        
     }
 }
