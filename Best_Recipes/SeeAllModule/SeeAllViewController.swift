270b210a9dc584cb6ef759ab86a9a4109b1ae7c2
Skitt2 <provaid@yandex.ru>
2024-07-13 13:46:27 +0300

    Коммит на SeeAllControllerImpl - доработка

--- Best_Recipes/SeeAllModule/SeeAllViewController.swift
+++ Best_Recipes/SeeAllModule/SeeAllViewController.swift
@@ -7,11 +7,11 @@ final class SeeAllControllerImpl: UIViewController {
     
     var presenter: (any SeeAllPresenter)?
     
-    private var isFirstLoad = true
-    var searchController: UISearchController?
-    private lazy var collectionView: UICollectionView = createCollectionView()
-    private lazy var dataSource: UICollectionViewDiffableDataSource<Section, AnyHashable> = createDataSource()
-    private var selectedIndexPaths: [Int: IndexPath] = [:]
+//    private var isFirstLoad = true
+//    var searchController: UISearchController?
+//    private lazy var collectionView: UICollectionView = createCollectionView()
+//    private lazy var dataSource: UICollectionViewDiffableDataSource<Section, AnyHashable> = createDataSource()
+//    private var selectedIndexPaths: [Int: IndexPath] = [:]
     
 }
 
